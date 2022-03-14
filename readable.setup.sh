710ffb7a4a2d484c6bee7d66bf7a624836ace2c4
Ehtesh Choudhury <ehtesh.choudhury@gmail.com>
2022-03-14 04:52:41 -0700

    comments and some reordering on stuff to first install

--- readable.setup.sh
+++ readable.setup.sh
@@ -1,5 +1,3 @@
-# https://github.com/nodesource/distributions/blob/master/README.md
-curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
 
 # third party binaries, installed manually
 : << 'END'
@@ -9,16 +7,25 @@ curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
 - sharkdp/fd  https://github.com/sharkdp/fd (find alternative)
 - sharkdp/bat https://github.com/sharkdp/bat (cat alternative with syntax highlighting)
 - sharkdp/vivid https://github.com/sharkdp/vivid (LS_COLORS for different file extensions)
-- https://github.com/sharkdp/diskus (du -sh alternative that's parallelized)
+- sharkdp/diskus https://github.com/sharkdp/diskus (du -sh alternative that's parallelized)
+- plt/racket
+- rustup
+- ghcup
+- ziglang
 END
 
-sudo apt install -y tmux mosh tree rsync ncdu htop fasd p7zip-full
-# for programming
-sudo apt -y install postgresql postgresql-client
-sudo apt install -y nodejs python3-pip tig
+sudo apt install -y tmux mosh tree rsync ncdu htop fasd tig p7zip-full
 
+# for programming
+sudo apt install -y postgresql postgresql-client
+
+# TODO install python 3.10
 sudo pip3 install ipython requests flask pylint black
 
+# https://github.com/nodesource/distributions/blob/master/README.md
+curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash -
+
+sudo apt install -y nodejs
 sudo npm install -g http-server serve nodemon
 sudo npm install -g typescript ts-node js-beautify eslint rimraf
 sudo npm install -g npm-check tldr #optional
