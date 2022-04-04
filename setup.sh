3230bc25b1895da6dad72845b5ab40d872546d63
Ehtesh Choudhury <ehtesh.choudhury@gmail.com>
2022-04-04 16:52:00 -0700

    clang and gcc installation instructions

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 
 # third party binaries, installed manually
-: << 'END'
+: << END
 - neovim https://github.com/neovim/neovim/wiki/Installing-Neovim#appimage-universal-linux-package
 - ripgrep https://github.com/BurntSushi/ripgrep#installation
 - scc https://github.com/boyter/scc
@@ -22,6 +22,18 @@ sudo apt install -y postgresql postgresql-client
 
 # TODO install python 3.10
 sudo pip3 install ipython requests flask pylint black
+
+# clang, gcc
+wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key | sudo apt-key add -
+sudo tee /etc/apt/sources.list.d/llvm.list << END
+# 14, https://apt.llvm.org/
+deb http://apt.llvm.org/focal/ llvm-toolchain-focal-14 main
+deb-src http://apt.llvm.org/focal/ llvm-toolchain-focal-14 main
+END
+# https://launchpad.net/~ubuntu-toolchain-r/+archive/ubuntu/test
+sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
+
+sudo apt update && sudo apt install clang-14 clang++-14 clangd-14 gcc-11
 
 # https://github.com/nodesource/distributions/blob/master/README.md
 curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash -
