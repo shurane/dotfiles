59d50b5315623328c94bdae08fd7ff8d724c142f
Ehtesh Choudhury <ehtesh@supio.com>
2025-12-10 22:26:20 -0800

    <leader>ff searches open files

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -68,6 +68,7 @@ return {
     priority = 1000,
     config = function()
       require("onedarkpro").setup()
+      -- vim.cmd([[colorscheme onedark]])
       vim.cmd([[colorscheme onedark_dark]])
     end,
   },
